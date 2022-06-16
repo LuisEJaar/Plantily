c2abfc43e15dbd6eb0d519e58f130284c00650a4
LuisEJaar <luisjaarcoding@gmail.com>
2022-06-16 13:07:37 -0500

    Issues getting data to show

--- server.js
+++ server.js
@@ -3,26 +3,40 @@ const bodyParser = require('body-parser')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 
-
-
-MongoClient.connect("mongodb+srv://luisjaar:mPoDy1DUi6nX54G3@cluster0.se0vt.mongodb.net/?retryWrites=true&w=majority", { useUnifiedTopology: true })
-    .then(client => {
-    console.log('Connected to Database')
-    })
-    .catch(error => console.error(error))
-
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html')
-    res.sendFile(__dirname + '/styles/normalize.css')
-    res.sendFile(__dirname + '/styles/style.css')
 })
 
-app.post("/plants", (req,res)=> {
-    console.log("plants page")
-    console.log(req.body)
-})
+const connectionString = "mongodb+srv://luisjaar:mPoDy1DUi6nX54G3@cluster0.se0vt.mongodb.net/?retryWrites=true&w=majority"
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then(client => {
+        const db = client.db('plantly-plants-entries')
+        const myGarden = db.collection('plants')
+
+        app.post("/plants", (req,res)=> {
+            myGarden.insertOne(req.body)
+                .then(result => {
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+        // app.use()
+        app.get('/', (req, res) => {
+            // db.collection('plants').find().toArray()
+            //   .then(results => {
+            //     console.log(results)
+            //   })
+            //   .catch(error => console.error(error))
+            const cursor = db.collection('plants').find()
+            console.log(cursor)
+            console.log("get not working")
+          })
+        // app.listen()
+    })
+    .catch(error => console.error(error))
 
 const PORT = 3000
 
