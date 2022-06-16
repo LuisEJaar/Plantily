455fc98b915c9b1accac0da0ba44f792c132f5a7
LuisEJaar <luisjaarcoding@gmail.com>
2022-06-16 14:12:23 -0500

    Added backend functionality. Need to modify for plantly

--- server.js
+++ server.js
@@ -1,46 +1,73 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const MongoClient = require('mongodb').MongoClient
+app.use(express.static('public'))
+app.use(bodyParser.json())
 
-app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html')
-})
-
-const connectionString = "mongodb+srv://luisjaar:mPoDy1DUi6nX54G3@cluster0.se0vt.mongodb.net/?retryWrites=true&w=majority"
-
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
-    .then(client => {
-        const db = client.db('plantly-plants-entries')
-        const myGarden = db.collection('plants')
-
-        app.post("/plants", (req,res)=> {
-            myGarden.insertOne(req.body)
-                .then(result => {
-                    res.redirect('/')
-                })
-                .catch(error => console.error(error))
-        })
-        // app.use()
-        app.get('/', (req, res) => {
-            // db.collection('plants').find().toArray()
-            //   .then(results => {
-            //     console.log(results)
-            //   })
-            //   .catch(error => console.error(error))
-            const cursor = db.collection('plants').find()
-            console.log(cursor)
-            console.log("get not working")
-          })
-        // app.listen()
-    })
-    .catch(error => console.error(error))
-
-const PORT = 3000
-
-app.listen(process.env.PORT || PORT, function () {
+app.listen(3000, function() {
     console.log('listening on 3000')
 })
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const MongoClient = require('mongodb').MongoClient
+
+let connectionString = "mongodb+srv://luisjaar:mPoDy1DUi6nX54G3@cluster0.se0vt.mongodb.net/?retryWrites=true&w=majority"
+let dbname = 'plantly-plants-entries'
+let collectionname = 'plants'
+let location = '/plants'
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true }) 
+.then(client => {
+    console.log('Connected to Database')
+    const db = client.db(dbname)
+    const quotesCollection = db.collection(collectionname)
+    app.set('view engine', 'ejs')
+    app.get('/', (req, res) => {
+        db.collection(collectionname).find().toArray()
+          .then(results => {
+            console.log(results)
+            res.render('index.ejs', { quotes: results })
+          })
+          .catch(error => console.error(error))
+    })
+    app.post(location, (req, res) => {
+        quotesCollection.insertOne(req.body)
+          .then(result => {
+            res.redirect('/')
+          })
+          .catch(error => console.error(error))
+    })
+    app.put(location, (req, res) => {
+      quotesCollection.findOneAndUpdate(
+        { name: 'Yoda' },
+        {
+          $set: {
+            name: req.body.name,
+            quote: req.body.quote
+          }
+        },
+        {
+          upsert: true
+        }
+      )
+      .then(result => {
+        console.log(result)
+       })
+      .catch(error => console.error(error))
+    })
+    app.delete(location, (req, res) => {
+      quotesCollection.deleteOne(
+        { name: req.body.name }
+      )
+      .then(result => {
+        if (result.deletedCount === 0) {
+          return res.json('No plant to delete')
+        }
+        res.json(`Deleted`)
+      })
+      .catch(error => console.error(error))
+    })
+})
+.catch(error => console.error(error))
