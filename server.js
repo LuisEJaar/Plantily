57b4a063d0ce9f84881f479bbc15591dfb6376bc
LuisEJaar <luisjaarcoding@gmail.com>
2022-06-16 12:20:55 -0500

    connected to mongo

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
+
+
+
+MongoClient.connect("mongodb+srv://luisjaar:mPoDy1DUi6nX54G3@cluster0.se0vt.mongodb.net/?retryWrites=true&w=majority", { useUnifiedTopology: true })
+    .then(client => {
+    console.log('Connected to Database')
+    })
+    .catch(error => console.error(error))
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function(req, res){
-    res.send("Good job")
+    res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/styles/normalize.css')
+    res.sendFile(__dirname + '/styles/style.css')
 })
 
-app.listen(3000, function () {
+app.post("/plants", (req,res)=> {
+    console.log("plants page")
+    console.log(req.body)
+})
+
+const PORT = 3000
+
+app.listen(process.env.PORT || PORT, function () {
     console.log('listening on 3000')
 })
+
